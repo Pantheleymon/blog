7d02130fa73c37f2ec7842273039a6467d232887
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-03-15 17:15:37 +0300

    feat: Удалить пост

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ app.post('/auth/register', registerValidation, UserController.register);
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
-// app.get('/posts', PostController.remove);
-// app.get('/posts', PostController.patch);
-// app.get('/posts', PostController.update);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+// app.patch('/posts', PostController.patch);
+// app.update('/posts', PostController.update);
 
 app.listen(4444, (err) => {
     if (err) {

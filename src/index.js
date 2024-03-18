bfa5649d334abc14ad8630343c049f4971b9a68e
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-03-18 17:28:07 +0300

    feat: Redux store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
-import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -15,7 +18,9 @@ root.render(
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
